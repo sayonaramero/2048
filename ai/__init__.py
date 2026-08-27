d2333c4223beeb19d2a981b2a11c53057f24e8a2
Periodonite <od.pedromega@gmail.com>
2026-08-27 20:46:20 -0300

    Working, WORKING!!!

--- ai/__init__.py
+++ ai/__init__.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 from torch import optim
 from colorama import Fore, Style
+from random import randint
 
 # -------- Constants -------- #
 
@@ -15,6 +16,13 @@ FALLBACK_DEVICE = "cpu"
 
 # --------------------------- #
 
+DEFAULT_GAMMA = 0.85
+
+# --------------------------- #
+
+DEFAULT_OPTIMIZER = optim.SGD
+DEFAULT_ERROR_CRITERION = nn.MSELoss
+
 print(f"{Fore.GREEN}-- CUDA Available ({Fore.BLUE}{torch.cuda.get_device_name(0)}{Fore.GREEN}) --{Style.RESET_ALL}" if torch.cuda.is_available() else f"{Fore.RED}-- CUDA Unavailable, using fallback device ({Fore.BLUE}{FALLBACK_DEVICE}{Fore.RED}) --{Style.RESET_ALL}")
 
 device = torch.device("cuda" if torch.cuda.is_available() else FALLBACK_DEVICE)
@@ -22,9 +30,34 @@ device = torch.device("cuda" if torch.cuda.is_available() else FALLBACK_DEVICE)
 class model(nn.Module):
     def __init__(self):
         super(model, self).__init__()
-        self.layers: list[nn.Linear | nn.ReLU] = [nn.Linear(INPUT_LAYER_DIM, HIDDEN_LAYER_DIM), nn.ReLU(), nn.Linear(HIDDEN_LAYER_DIM, OUTPUT_LAYER_DIM)]
+        self.layers = nn.ModuleList([
+            nn.Linear(INPUT_LAYER_DIM, HIDDEN_LAYER_DIM),
+            nn.ReLU(),
+            nn.Linear(HIDDEN_LAYER_DIM, OUTPUT_LAYER_DIM),
+        ])
     def forward(self, input: torch.Tensor):
         out = input
         for i, layer in enumerate(self.layers):
             out = layer(out)
+        #print(out)
         return out
+    def choose(self, output: torch.Tensor):
+        with torch.no_grad():
+            actions = torch.argmax(output, dim=1)
+        random_actions = torch.randint(0, OUTPUT_LAYER_DIM, size=(len(output),))
+        do_random_choice = torch.rand(len(output)) > 0.85
+        choosen_actions = torch.where(do_random_choice, random_actions, actions)
+        return choosen_actions
+    def calculate_loss(self, legacy_q_values, next_state: torch.Tensor, reward: float, criterion) -> torch.Tensor:
+        '''
+        model_out_t1 2nd Prediction
+        '''
+        with torch.no_grad():
+            next_prediction = self(next_state)
+            print(torch.max(next_prediction, dim=1))
+            target = reward + DEFAULT_GAMMA * torch.max(next_prediction, dim=1)[0]
+
+        #print(f"-----\n{next_prediction}")
+        print(f"Predicted Value: {legacy_q_values}\n---\nCorrect Value {target}")
+        loss = criterion(legacy_q_values, target)
+        return loss
